ad3296accbdd9940102f72b01d86da026b920935
elc1d <francusant@gmail.com>
2024-02-25 00:03:28 -0500

    Update traverseDependencies9.7.8.sh

--- traverseDependencies9.7.8.sh
+++ traverseDependencies9.7.8.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+o#!/bin/sh
 echo "Step 1: Updating and Upgrading OS:"
 dnf update -y && dnf upgrade -y
+echo ""
+echo ""
 echo "Step 2: Downloading traverse:"
 cd ~/Downloads/
 wget --no-check-certificate https://download.kaseya.com/components/traverse/v9.7.0.0/453/traverse-9.7.8-linux-alma.tar.gz
 sha1sum traverse-9.7.8-linux-alma.tar.gz
 echo "660d0432a0c97884dfff9894c53a08697f1e4ff1  traverse-9.7.8-linux-alma.tar.gz -- Does it match? If not stop the installation"
+echo ""
+echo ""
 echo "Step 3: Installing Dependencies"
 yum -y install curl 
 yum -y install wget 
