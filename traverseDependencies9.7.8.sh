4b1e842efa00aaffed97a82ef975e266b6e649de
elc1d <francusant@gmail.com>
2024-02-26 23:20:56 -0500

    Update traverseDependencies9.7.8.sh

--- traverseDependencies9.7.8.sh
+++ traverseDependencies9.7.8.sh
@@ -2,7 +2,14 @@
 
 # Script created by Francisco Santistevan on 2024-02-26
 
-echo "Script created by Francisco Santistevan on 2024-02-26"
+echo "(`-')        (`-')  (`-')  _       (`-') (`-')  _ (`-').->(`-')                          (`-')"                 
+echo "( OO).->  <-.(OO )  (OO ).-/      _(OO ) ( OO).-/ ( OO)_  ( OO).->       .->            _(OO )"                 
+echo "/    '._  ,------,) / ,---.  ,--.(_/,-.\(,------.(_)--\_) /    '._   ,--.'  ,-.    ,--.(_/,-.\ .--.    .----."  
+echo "|'--...__)|   /`. ' | \ /`.\ \   \ / (_/ |  .---'/    _ / |'--...__)(`-')'.'  /    \   \ / (_//_  |   \_,-.  |"
+echo "`--.  .--'|  |_.' | '-'|_.' | \   /   / (|  '--. \_..`--. `--.  .--'(OO \    /      \   /   /  |  |      .' .'"
+echo "   |  |   |  .   .'(|  .-.  |_ \     /_) |  .--' .-._)   \   |  |    |  /   /)     _ \     /_) |  |    .'  /_"
+echo "   |  |   |  |\  \  |  | |  |\-'\   /    |  `---.\       /   |  |    `-/   /`      \-'\   /    |  |,-.|      |"
+echo "   `--'   `--' '--' `--' `--'    `-'     `------' `-----'    `--'      `--'            `-'     `--''-'`------'"
 echo ""
 
 echo "Step 1: Updating and Upgrading OS:"
@@ -19,31 +26,7 @@ echo ""
 echo ""
 
 echo "Step 3: Installing Dependencies"
-echo "Installing wget"
-yum -y install wget 
-echo "Installing tar"
-yum -y install tar
-echo "Installing zip"
-yum -y install zip 
-echo "Installing popt.i686"
-yum -y install popt.i686 
-echo "Installing zlib.i686"
-yum -y install zlib.i686
-echo "Installing expat-devel.i686"
-yum -y install expat-devel.i686
-echo "Installing glib2.i686"
-yum -y install glib2.i686
-echo "Installing libnsl.i686"
-yum -y install libnsl.i686
-echo "Installing libxcrypt-compat.i686"
-yum -y install libxcrypt-compat.i686
-echo "Installing libicu.x86_64"
-yum -y install libicu.x86_64
-echo "Installing libstdc++.so.6"
-yum -y install libstdc++.so.6
-echo "Installing fontconfig.x86_64"
-yum -y install fontconfig.x86_64
-echo ""
+dnf -y install wget tar zip popt.i686 zlib.i686 expat-devel.i686 glib2.i686 libnsl.i686 libxcrypt-compat.i686 libicu.x86_64 libstdc++.so.6 fontconfig.x86_64
 echo ""
 echo ""
 
@@ -68,55 +51,3 @@ echo ""
 
 echo "Please reboot the system to apply changes."
 echo ""
-
-
-
-
-
-
-
-
-
-
-
-
-
-#!/bin/sh
-echo "Step 1: Updating and Upgrading OS:"
-dnf update -y && dnf upgrade -y
-echo ""
-echo ""
-echo "Step 2: Creating and changing Directories to ~/Downloads/"
-cd ~/
-mkdir Downloads
-cd ~/Downloads/
-pwd
-echo ""
-echo ""
-echo "Step 3: Installing Dependencies"
-yum -y install wget 
-yum -y install tar
-yum -y install zip 
-yum -y install popt.i686 
-yum -y install zlib.i686
-yum -y install expat-devel.i686
-yum -y install glib2.i686
-yum -y install libnsl.i686
-yum -y install libxcrypt-compat.i686
-yum -y install libicu.x86_64
-yum -y install libstdc++.so.6
-yum -y install fontconfig.x86_64
-echo ""
-echo ""
-echo "Step 4: Downloading Traverse-9.7.8"
-wget --no-check-certificate https://download.kaseya.com/components/traverse/v9.7.0.0/453/traverse-9.7.8-linux-alma.tar.gz
-sha1sum traverse-9.7.8-linux-alma.tar.gz > traverse_download_sha1sum.log
-echo ""
-echo ""
-echo "Step 5: Extracting Traverse:"
-gunzip -c traverse-9.7.8-linux-alma.tar.gz | tar xpf -
-cd ~/Downloads/traverse9.7/
-pwd
-echo ""
-echo ""
-echo "Run sh ./install.sh to get the installation started."
